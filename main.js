defb5503e648cf341fb94d669c0b77043a064487
Tianaah <christiana15ako@gmail.com>
2025-09-21 14:40:59 +0100

    Generated report summary section and added to main.js

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-function removeProduct(name){
-    for(let i=0; i<inventory.length; i++){
-        if (inventory[i].name == name){
-            inventory.splice(i,1);
-            return;
-        }
+function removeProduct(name) {
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].name == name) {
+      inventory.splice(i, 1);
+      return;
     }
-    console.log(`${name} doesnot exist in inventory`);
+  }
+  console.log(`${name} doesnot exist in inventory`);
 }
 
 removeProduct("apple");
 removeProduct("bag");
 console.log("Your inventory after removing items: ", inventory, "\n");
+
+const generateReportSummary = () => {
+  let totalProducts = inventory.length;
+  let totalValue = 0;
+
+  for (let i = 0; i < inventory.length; i++) {
+    totalValue += inventory[i].price * inventory[i].quantity;
+  }
+
+  console.log("\n=== Inventory Summary ===");
+  console.log("Total number of products:", totalProducts);
+  console.log("Total inventory value:", totalValue);
+  console.log("========================\n");
+};
