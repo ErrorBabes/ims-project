b51aa3af7fa7f5569eb40db8b549c545fda3bc29
Olamayorrh <105288976+Olamayorrh@users.noreply.github.com>
2025-09-21 16:31:18 +0100

    codes to determine low stock product

--- main.js
+++ main.js
@@ -16,11 +16,31 @@ const generateReportSummary = () => {
   let totalProducts = inventory.length;
   let totalValue = 0;
 
+  let lowStock = [];
+
   for (let i = 0; i < inventory.length; i++) {
     totalValue += inventory[i].price * inventory[i].quantity;
+
+    let product = inventory[i];
+
+    //Determining of product quantity is low
+    if (product.productQuantity < 10) {
+      lowStock.push(product.productName);
+      console.log(
+        ` Low stock alert for product ${product.productName}! with ${product.productQuantity} quantity`
+      );
+    }
   }
 
-  console.log("\nInventory Summary");
+  console.log("\n ****** Inventory Summary ******");
+
+  //list(s) of low stock items
+  if (lowStock.length > 0) {
+    console.log(` low Stock Products: ${lowStock.join(",")} \n\n`);
+  }
+
   console.log("Total number of products:", totalProducts);
   console.log("Total inventory value:", totalValue, "\n");
 };
+
+generateReportSummary();
